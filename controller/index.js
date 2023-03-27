8459a8267ec2fccb8320fea03213c783191fd83c
Osamuyi <muyixone@yahoo.com>
2023-03-28 00:04:39 +0100

    Created the update note functionality

--- controller/index.js
+++ controller/index.js
@@ -1,14 +1,16 @@
 const notesModel = require('../models/notes_model');
 
-const getHomepage = async (req, res, next) => {
-  await notesModel.findOne({}).then((result) => {
-    res.status(200).json({
-      notes: result,
-      message: 'Result fetch complete',
+const getHomepage = async (req, res, next) => {};
+
+//Get a single note
+const getNote = async (req, res, next) => {
+  await notesModel.findOne({ _id: req.params.id }).then((result) => {
+    return res.status(200).json({
+      note: result,
+      message: 'Result fetched successfully',
     });
   });
 };
-const getNote = (req, res, next) => {};
 const createNote = async (req, res, next) => {
   // ADD INPUT VALIDATION WITH JOI HERE
 
@@ -21,7 +23,23 @@ const createNote = async (req, res, next) => {
     });
   });
 };
-const updateNote = (req, res, next) => {};
+const updateNote = async (req, res, next) => {
+  const noteId = req.params.id;
+  const updateNote = req.body;
+  await notesModel
+    .findOneAndUpdate({ _id: noteId }, { $set: updateNote }, { new: true })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({ message: 'Document not found' });
+      }
+      res.status(200).json({
+        result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'No document to update' });
+    });
+};
 const deleteNote = (req, res, next) => {};
 
 module.exports = {
